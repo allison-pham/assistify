e45bb0ac0679220876b7efdd66915f19ab6577ca
Luke <lukedmcalpine@gmail.com>
2024-08-10 16:46:10 -0700

    take user input and display response

--- chatbot.js
+++ chatbot.js
@@ -1,28 +1,3 @@
-// The Readline module allows reading of input stream line by line
-//const readline = require("node:readline");
-
-// Create an instance of a readline interface which allows for command-line input and output
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
-
-// rl.question(`Hello, how can I help you? Press 1 for technical interview preparation, press 2 for technical support, press 3 for account management. `, (selection) => {
-//     if (selection === "1") {
-//         console.log("You have selected technical interview preparation.");
-//     }
-//     else if (selection === "2") {
-//         console.log("You have selected technical support.");
-//     }
-//     else if (selection === "3") {
-//         console.log("You have selected account management.");
-//     } else {
-//         console.log("Please enter a valid response.");
-//     }
-//     rl.close();
-// });
-
-
 let button = document.getElementById("submitButton");
 button.addEventListener('click', function () {
     let text = document.getElementById("message").value;
@@ -30,4 +5,23 @@ button.addEventListener('click', function () {
 
     let displayArea = document.getElementById("displayArea");
     displayArea.textContent = text;
+
+    let botText = document.getElementById("botText");
+    if (text === "1") {
+
+        botText.textContent = "You have selected technical interview preparation.";
+    }
+    else if (text === "2") {
+
+        botText.textContent = "You have selected technical support.";
+    }
+
+    else if (text === "3") {
+
+        botText.textContent = "You have selected account management.";
+    }
+    else {
+
+        botText.textContent = "Please enter a valid response.";
+    }
 })
