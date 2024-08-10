d1a3a505baf72ef5412305861e03a78421499a90
Luke <lukedmcalpine@gmail.com>
2024-08-10 15:04:52 -0700

    Prompt user for response and display the selection

--- chatbot.js
+++ chatbot.js
@@ -3,11 +3,13 @@ const readline = require("node:readline");
 
 // Create an instance of a readline interface which allows for command-line input and output
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
-rl.question(`What's your name? `, (name) => {
-  console.log(`Hi ${name}!`);
-  rl.close();
+rl.question(`Hello, how can I help you? Press 1 for technical interview preparation `, (selection) => {
+    if (selection === "1") {
+        console.log("You have selected technical interview preparation");
+    }
+    rl.close();
 });
