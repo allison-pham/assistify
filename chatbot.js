c95569ae432dbb2e445e8c94ec8a1c91e82544ab
Luke <lukedmcalpine@gmail.com>
2024-08-10 15:10:13 -0700

    add options for technical support and account management

--- chatbot.js
+++ chatbot.js
@@ -7,9 +7,17 @@ const rl = readline.createInterface({
     output: process.stdout,
 });
 
-rl.question(`Hello, how can I help you? Press 1 for technical interview preparation `, (selection) => {
+rl.question(`Hello, how can I help you? Press 1 for technical interview preparation, press 2 for technical support, press 3 for account management. `, (selection) => {
     if (selection === "1") {
-        console.log("You have selected technical interview preparation");
+        console.log("You have selected technical interview preparation.");
+    }
+    else if (selection === "2") {
+        console.log("You have selected technical support.");
+    }
+    else if (selection === "3") {
+        console.log("You have selected account management.");
+    } else {
+        console.log("Please enter a valid response.");
     }
     rl.close();
 });
